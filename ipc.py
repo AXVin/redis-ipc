be8d82f06a6cc08236bc57b5ee97d29b10457c4d
AXVin <akash.vinayak.691@gmail.com>
2021-05-22 16:41:07 +0530

    suffering continues

--- ipc.py
+++ ipc.py
@@ -98,11 +98,14 @@ class IPC:
     async def listen_ipc(self):
         try:
             await self.ensure_channel()
-            while True:
-            #async for message in self.channel.listen():
-                message = await self.channel.get_message(ignore_subscribe_messages=True)
-                message = json.loads(message)
+            #while True:
+            async for message in self.channel.listen():
+                # message = await self.channel.get_message(ignore_subscribe_messages=True)
+                if message.get("type") == "subscribe":
+                    continue
                 print(message)
+                # message = json.loads(message)
+                # print(message)
                 op = message.pop("op")
                 nonce = message.pop("nonce", None)
                 sender = message.pop("sender", None)
