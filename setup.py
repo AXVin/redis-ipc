90eb531036298c476788cb4be444b4d68eae7578
AXVin <akash.vinayak.691@gmail.com>
2021-06-27 20:29:10 +0530

    Adds License

--- setup.py
+++ setup.py
@@ -4,17 +4,24 @@ requirements = []
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
+with open("README.md") as f:
+    long_description = f.read()
+
 setup(
     name='redis-ipc',
     description='A minimal multi producer single consumer IPC using redis pub/sub',
     author='AXVin',
     url='https://github.com/AXVin/redis-ipc',
-    keywords=['redis', 'ipc'],
+    keywords=['aioredis', 'redis', 'ipc'],
     install_requires=requirements,
     classifiers=[
         'Development Status :: 1 - Alpha',
         'Intended Audience :: Developers',
         'Programming Language :: Python :: 3.8',
+        'Operating System :: OS Independent',
     ],
     packages=['redisipc'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    python_requires=">=3.8"
 )
